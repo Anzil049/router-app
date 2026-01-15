40565c25fe87f53e22f70fc0971907e5df975b26
anzil049 <anzil049@gmail.com>
2026-01-15 12:48:04 +0530

    completion

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import NotFound from "./pages/NotFound";
 
+import DashboardLayout from "./components/DashboardLayout";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <div>
@@ -16,6 +21,20 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:id" element={<ProjectDetails />} />
         <Route path="/contact" element={<Contact />} />
+
+        {/* 🔐 Protected Nested Routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
